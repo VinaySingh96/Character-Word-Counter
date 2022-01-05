748184031512117ce09d4611795b25adabff4d6e
Vinay <vinayaksingh920@gmail.com>
2022-01-06 03:17:05 +0530

    refactored

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-black" >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">{props.title}</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <a className="navbar-brand" style={{color:"white"}} href="/" >{props.title}</a>
+                    <button style={{backgroundColor:"white"}} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span style={{color:"white"}} className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse" id="navbarNav" >
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className="nav-link active" style={{color:"white"}} aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Features</a>
+                                <a className="nav-link" style={{color:"white"}} href="/">Features</a>
                             </li>
                             
                         </ul>
--- src/Textbox.js
+++ src/Textbox.js
@@ -17,22 +17,27 @@ export default function Textbox() {
   let words=0;
   for(let i=0;i<text.length;i++)
   {
-    if(text[i]==' ')
+    if(text[i]===' ')
     words++;
   }
   words++;
-  if(text.charAt(text.length-1)==' ')
+  if(text.charAt(text.length-1)===' ')
   words--;
-  if(text.length==0)
+  if(text.length===0)
   words=0;
+
+  const clearText=()=>{
+    setText('');
+  }
   
   return (
     <>
       <div className="container">
       <h3>Count word/Character in your Text</h3>
       <textarea className="form-control" value={text} onChange={onChange} id="myBox" rows="10" ></textarea>
-      <button className="btn btn-primary my-3 " onClick={toUpper} >To Uppercase</button>
-      <button className="btn btn-primary my-3 mx-3" onClick={toLower} >To Lowercase</button>
+      <button className="btn btn-success my-3 " onClick={toUpper} >To Uppercase</button>
+      <button className="btn btn-success my-3 mx-3" onClick={toLower} >To Lowercase</button>
+      <button className="btn btn-success my-3 " onClick={clearText} >Clear Text</button>
       <p>No. of Characters in the Text : {text.length}</p>
       <p>No. of Words in the Text : {words}</p>
       </div>
